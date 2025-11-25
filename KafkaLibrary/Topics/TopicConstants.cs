2137f8b65f8adca6c5f69c2ee935371314ded67c
dammirka <cdbacjdorz@yandex.ru>
2025-11-25 10:45:46 +0300

    added topics

---
+++ KafkaLibrary/Topics/TopicConstants.cs
@@ -0,0 +1,19 @@
+﻿namespace KafkaLibrary.Topics;
+
+public static class TopicConstants
+{
+    /// <summary>
+    /// Топик для отправки в Elibrary
+    /// </summary>
+    public static string ELibraryTopic = "elibrary-topic";
+
+    /// <summary>
+    /// Топик для обновленной статьи
+    /// </summary>
+    public static string ArticleUpdateEventTopic = "article-updated";
+    
+    /// <summary>
+    /// Топик для сохранения данных в postgres
+    /// </summary>
+    public static string DataSaverTopic = "data-saver-topic";
+}
